0284e53a8550c76ade8182a5b291e77bf4c5cbbe
James Ahrens <jamesahrens@Jamess-MacBook-Pro.local>
2018-12-16 15:25:07 -0500

    activity indicator and image with opqueue in instructions

---
+++ snake/InstructionsViewController.swift
@@ -0,0 +1,54 @@
+//
+//  InstructionsViewController.swift
+//  snake
+//
+//  Created by James Ahrens on 12/16/18.
+//  Copyright © 2018 James Ahrens. All rights reserved.
+//
+
+import UIKit
+
+class InstructionsViewController: UIViewController {
+
+    @IBOutlet weak var myImageView: UIImageView!
+    
+    @IBOutlet weak var activityIndicator : UIActivityIndicatorView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        activityIndicator.hidesWhenStopped = true
+        activityIndicator.center = myImageView.center
+        activityIndicator.startAnimating()
+        view.addSubview(activityIndicator)
+        
+        let queue = OperationQueue()
+        let urlString = "https://cdn.pixabay.com/photo/2013/07/12/12/15/snake-145410_1280.png"
+        let op = myOp(myUrl: urlString)
+        op.completionBlock = {
+            OperationQueue.main.addOperation({
+                if let downloadedImage = op.image {
+                    //self.images.append(downloadedImage)
+                    //self.images[i] = downloadedImage
+                    self.myImageView.image = downloadedImage
+                }
+                self.activityIndicator.stopAnimating()
+            })
+            
+        }
+        queue.addOperation(op)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
